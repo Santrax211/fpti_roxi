ee415f20416a4c49b2f50f2e32d8822897127b3c
Claudio2k <claudioramon153@gmail.com>
2025-06-06 06:06:12 -0500

    Revert "Cambios de diseño"
    This reverts commit f433451fe58e9e59409dd5c3e660914226651292.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-// Añade esta línea para que TS trate cualquier import terminado en “.astro” como módulo válido de tipo any:
-declare module '*.astro'
